b28b03f254b67554403e021d266a4b774e31954a
hecto <hecto@LENOVO>
2021-02-13 19:42:25 -0600

    Agregar archivos de proyecto.

---
+++ DB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace EF_Core_PostgreSQL
+{
+    class DB: DbContext
+    {
+
+        private readonly string schema;
+
+        public AppDatabase(string schema)
+          : base("AppDatabaseConnectionString")
+        {
+            this.schema = schema;
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            builder.HasDefaultSchema(this.schema);
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
